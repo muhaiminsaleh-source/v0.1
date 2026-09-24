b31b17681a109c8582e7b6aed20b27944c8fd80e
muhaiminsaleh-source <muhaimin.saleh@vvk.lt>
2026-09-24 15:20:51 +0300

    Add Person class declaration

--- Person.h
+++ Person.h
@@ -17,29 +17,21 @@ private:
     double finalGrade;
 
 public:
-    // Constructor
     Person();
 
-    // Copy constructor
     Person(const Person& other);
 
-    // Assignment-copy operator
     Person& operator=(const Person& other);
 
-    // Destructor
     ~Person();
 
-    // Input and output operators
     friend istream& operator>>(istream& in, Person& person);
     friend ostream& operator<<(ostream& out, const Person& person);
 
-    // Calculate final grade using average
     double calculateAverage() const;
 
-    // Calculate final grade using median
     double calculateMedian() const;
 
-    // Get student information
     string getFirstName() const;
     string getSurname() const;
     double getFinalGrade() const;
